1195dbc2c5202f1d1c22e40b3a628c7291532c95
islomnumanov <islom4ik.numanov@gmail.com>
2021-07-07 12:16:36 +0900

    D20 hangman

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const products = {
-    name: 'something'
-}
+// Primitive value: string, number, boolean, null, undefined -------> anything except for these in JavaScript is considered to be an Object
 
-// hasOwnProperty
-console.log(products.hasOwnProperty('toLowerCase'));
+// Object: myObject --> Object.prototype --> null
+// Array: myArray --> Array.prototype --> Object.prototype --> null
+// Function: myFunc --> Function.prototype --> Object.prototype --> null
+
+// Among Primitive Values below three have Object Wrappers i.e. can be converted to an Object then accessed to their methods. Null and Undefined will never be an Object!
+// String: myString --> String.prototype --> Object.prototype --> null
+// Number: myNumber --> Number.prototype --> Object.prototype --> null
+// Boolean: myBoolean --> Boolean.prototype --> Object.prototype --> null
+const product = 'Computer'
+console.log(product);
+
+const otherProduct = new String('phone')
+console.log(otherProduct);
