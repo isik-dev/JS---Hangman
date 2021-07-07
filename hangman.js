7a1c5c0444fbdbf4a6fe142c547e32183b77245a
islomnumanov <islom4ik.numanov@gmail.com>
2021-07-07 13:56:25 +0900

    Added: makeGuess method & keypress eventListener

--- hangman.js
+++ hangman.js
@@ -6,11 +6,22 @@
 // 2. Set up another instance property to store guessed letters
 // 3. Create a method that gives you the word puzzle back
 
+// Create a method for making a guess
+// 1. Should accept a character for guessing
+// 2. Should add unique guesses to list of guesses
+// 3. Should decrement the guesses left if a unique guess isn't a match
+
+// 1. Display the puzzle to the browser instead of the console
+// 2. Display the guesses left to the browser instead of console
+// 3. Seperate the Hangman definition from the rest of the app code (use app.js)
+
 const Hangman = function (word, guessesRemaining) {
     this.word = word.toLowerCase().split("")
     this.guessesRemaining = guessesRemaining
-    this.guessedLetters = ['l','s']
-}
+    this.guessedLetters = []
+  
+    }
+
 
 Hangman.prototype.getPuzzle = function () {
     let puzzle = ''
@@ -26,5 +37,29 @@ Hangman.prototype.getPuzzle = function () {
     return puzzle
 }
 
-const me = new Hangman('Alex Sanchez', 22)
+Hangman.prototype.makeGuess = function (guess) {
+    guess = guess.toLowerCase()
+    const uniqueGuess = !this.guessedLetters.includes(guess)
+    const badGuess = !this.word.includes(guess)
+
+    if (uniqueGuess) {
+        this.guessedLetters.push(guess)
+    }
+    if (uniqueGuess && badGuess) {
+        this.guessesRemaining--
+    }
+}
+
+
+
+const me = new Hangman('Alex Sanchez', 2)
+
 console.log(me.getPuzzle());
+console.log(me.guessesRemaining);
+
+window.addEventListener('keypress', function (e) {
+    const guess = String.fromCharCode(e.charCode)
+    me.makeGuess(guess)
+    console.log(me.getPuzzle());
+    console.log(me.guessesRemaining);
+})
