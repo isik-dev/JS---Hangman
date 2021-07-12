4b39212e8b058efcd4e13c5f8830df3d4e90e92b
islomnumanov <islom4ik.numanov@gmail.com>
2021-07-12 16:31:25 +0900

    API requests made

--- app.js
+++ app.js
@@ -32,18 +32,37 @@ window.addEventListener('keypress', (e) => {
 
 
 // Making an HTTP request
-const request = new XMLHttpRequest()
+// const request = new XMLHttpRequest()
 
-request.addEventListener('readystatechange', (e) => {
+// request.addEventListener('readystatechange', (e) => {
+//     if (e.target.readyState === 4 && e.target.status === 200) {
+//         const data = JSON.parse(e.target.responseText)
+//         console.log(data);
+//     } else if (e.target.readyState === 4) {
+//         console.log('An error has taken place');
+//     }
+// })
+
+// request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=2')
+// request.send()
+
+// 1. Make a new request for all countries
+// 2. Parse the responseText to get back the array of objects
+// 3. Find your country object by its country code (alpha2Code property)
+// 4. Print the full country name (name property)
+
+const countryCode = 'KR'
+const countryRequest = new XMLHttpRequest()
+
+countryRequest.addEventListener('readystatechange', (e) => {
     if (e.target.readyState === 4 && e.target.status === 200) {
         const data = JSON.parse(e.target.responseText)
-        console.log(data);
+        const country = data.find((country) => country.alpha2Code === countryCode)
+        console.log(country.name);
     } else if (e.target.readyState === 4) {
-        console.log('An error has taken place');
+        console.log('Unable to fetch data');
     }
 })
 
-request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=2')
-request.send()
-
-
+countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all')
+countryRequest.send()
