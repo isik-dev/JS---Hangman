ab52e2dbbeeb6e01e76a628d593c749f31416f00
islomnumanov <islom4ik.numanov@gmail.com>
2021-07-12 15:30:52 +0900

    making HTTP request using XMLHttpRequest

--- app.js
+++ app.js
@@ -8,6 +8,11 @@
 // String: myString --> String.prototype --> Object.prototype --> null
 // Number: myNumber --> Number.prototype --> Object.prototype --> null
 // Boolean: myBoolean --> Boolean.prototype --> Object.prototype --> null
+
+// HTTP (Hypertext Transfer Protocol)
+// Request = what do we want to do
+// Respons = What was actually done
+
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guessesRemaining')
 const game1 = new Hangman('Davidson LLC', 2)
@@ -17,11 +22,26 @@ puzzleEl.textContent = game1.puzzle
 guessesEl.textContent = game1.statusMessage
 console.log(game1.status);
 
-window.addEventListener('keypress', function (e) {
+window.addEventListener('keypress', (e) => {
     const guess = String.fromCharCode(e.charCode)
     game1.makeGuess(guess)
     puzzleEl.textContent = game1.puzzle
     guessesEl.textContent = game1.statusMessage
     console.log(game1.status);
 })
+
+
+// Making an HTTP request
+const request = new XMLHttpRequest()
+
+request.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4) {
+        const data = JSON.parse(e.target.responseText)
+        console.log(data);
+    }
+})
+
+request.open('GET', 'http://puzzle.mead.io/puzzle')
+request.send()
+
 
